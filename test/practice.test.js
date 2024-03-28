6021576bcc64e8c63edc0130a0a21c942be19dfa
Mustafa Özkara <mustafaozkara@outlook.fr>
2024-03-28 17:15:54 +0100

    Add test for analyse array function

--- test/practice.test.js
+++ test/practice.test.js
@@ -2,6 +2,7 @@ import capitalize from "../js/capitalize";
 import reverse from "../js/reverse";
 import calculator from "../js/calculator";
 import caesarCipher from "../js/caesarCipher";
+import analyzeArray from "../js/analyzeArray";
 
 test("A capitalize function that takes a string and returns it with the first character capitalized", () => {
   expect(capitalize("this is a test")).toBe("This is a test");
@@ -29,4 +30,13 @@ test("An add function that takes two numbers and then return them divided", () =
 
 test("A caesar Cipher function that take a string and a shift factor and return the string with each character shifted", () => {
   expect(caesarCipher("mus.!tafa.", 3)).toBe("PXV.!WDID.");
+});
+
+test("An analyze array function that take an array of numbers and return an object with 4 properties,", () => {
+  expect(analyzeArray([1, 8, 3, 4, 2, 6])).toStrictEqual({
+    average: 4,
+    min: 1,
+    max: 8,
+    length: 6,
+  });
 });
