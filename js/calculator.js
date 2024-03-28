e610a2d2398a173a7a99d677bee0f7f72809a739
Mustafa Özkara <mustafaozkara@outlook.fr>
2024-03-28 15:53:53 +0100

    Add basic maths operations and export it into in an object

---
+++ js/calculator.js
@@ -0,0 +1,9 @@
+const add = (firstNumber, secondNumber) => firstNumber + secondNumber;
+
+const subtract = (firstNumber, secondNumber) => firstNumber - secondNumber;
+
+const multiply = (firstNumber, secondNumber) => firstNumber * secondNumber;
+
+const divide = (firstNumber, secondNumber) => firstNumber / secondNumber;
+
+export default { add, subtract, multiply, divide };
